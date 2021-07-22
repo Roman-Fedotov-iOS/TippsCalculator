7419fa33221622512abe50bf69ed27ec830b88e3
Roman-Fedotov-iOS <romafed23@gmail.com>
2021-07-22 23:02:29 +0300

    Initial Commit

---
+++ TippsCalculator/TippsCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TippsCalculatorApp.swift
+//  TippsCalculator
+//
+//  Created by Roman Fedotov on 22.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct TippsCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
