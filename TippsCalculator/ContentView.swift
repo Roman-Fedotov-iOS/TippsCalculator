57a9b7a0a5632c53abcab2c8b136dcf8ffe25c04
Roman-Fedotov-iOS <romafed23@gmail.com>
2021-07-26 14:17:34 +0300

    1

--- TippsCalculator/ContentView.swift
+++ TippsCalculator/ContentView.swift
@@ -8,9 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = 2
+    @State private var tipPercent = 0
+    
+    let tipPercents: [Int] = [0, 5, 10, 15, 20, 25]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercents[tipPercent])
+        let orderAmount = Double(checkAmount) ?? 0
+        let tipValue = orderAmount/100 * tipSelection
+        let grandTotal = orderAmount + tipValue
+        let amountPerPerson = grandTotal/peopleCount
+        return amountPerPerson
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", text: $checkAmount)
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<21) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                Section(header: Text("How many tipps?")) {
+                    Picker("Tip percent", selection: $tipPercent) {
+                        ForEach(0..<tipPercents.count) {
+                            Text("\(self.tipPercents[$0])%")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                Section {
+                    Text("\(totalPerPerson, specifier: "%.2f")")
+                }
+            }
+            .navigationBarTitle("Calculator For Tipps", displayMode: .inline)
+        }
     }
 }
 
